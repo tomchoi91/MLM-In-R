5d35b5e0f4b73595e96f658ae84e2b81b701cb44
Dongho (Tom) Choi <89372312+tomchoi91@users.noreply.github.com>
2023-01-24 11:42:48 -0600

    some changes made

--- Learning Checks/icc_tc.R
+++ Learning Checks/icc_tc.R
@@ -2,8 +2,8 @@ library(lme4)
 library(magrittr)
 library(dplyr)
 
-aa <- lme4::sleepstudy
-str(aa)
+# aa <- lme4::sleepstudy
+# str(aa)
 random.intercepts.model <- lmer(Reaction ~ Days + (1|Subject),  data=lme4::sleepstudy)
 summary(random.intercepts.model)
 VarCorr(random.intercepts.model)
@@ -14,8 +14,15 @@ VarCorr(random.intercepts.model)
 
 m2 <- lmer(Reaction~Days+(1|Subject)+(0+Days|Subject),lme4::sleepstudy,REML=FALSE)
 m1 <- update(m2,.~Days+(1|Subject))
-m0 <- lm(Reaction~Days,sleepstudy)
+m0 <- lm(Reaction~Days,lme4::sleepstudy)
 anova(m2,m1,m0) ## two sequential tests
+
+# m0a <- lmer(Reaction~Days+(1|Subject),lme4::sleepstudy)
+# summary(m0a)
+# m0b <- lmer(Reaction~1+(1|Subject),lme4::sleepstudy)
+# summary(m0b)
+# anova(m0a,m0b, refit=FALSE)
+# anova(m0a,m0b, refit=TRUE)
 
 
 VarCorr(random.intercepts.model) %>%
